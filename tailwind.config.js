38e8cb27d5bbd14616bb98220ee083a38955ed43
Pedrors0021 <pedrobachiega22@gmail.com>
2025-03-06 14:20:08 -0300

    Adicionando projeto ao GitHub

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: ["./*.html", "./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          red: {
+            300: "#fca5a5", // exemplo para text-red-300
+            // adicione outras tonalidades conforme necessário
+          },
+          gray: {
+            200: "#e5e7eb", // para text-gray-200
+            600: "#4b5563", // para text-gray-600
+            800: "#1f2937", // para hover:text-gray-800
+          },
+          green: {
+            100: "#d1fae5", // para text-green-100
+          },
+        },
+      },
+    },
+    plugins: [],
+  }
+  
